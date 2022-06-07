416abb0631d25377982d771e4b37603cd9abf229
Dmitrii Morozov <33morozov@gmail.com>
2022-06-07 11:21:06 +0300

    Refactoring gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -9,16 +9,19 @@ def generate_diff(file_1, file_2):
     for key in keys:
         val1 = in_file1.get(key)
         val2 = in_file2.get(key)
-        if val1 is not None and val2 is not None:
-            if val1 == val2:
-                result.append(f'   {key}: {val1}\n')
-            else:
-                result.append(f' - {key}: {val1}\n')
-                result.append(f' + {key}: {val2}\n')
-        elif val1 is None and val2 is not None:
-            result.append(f' + {key}: {val2}\n')
+        if val1 == val2:
+            add_str(result, key, val1, '   ')
+        elif val1 is None:
+            add_str(result, key, val2, ' + ')
+        elif val2 is None:
+            add_str(result, key, val1, ' - ')
         else:
-            result.append(f' - {key}: {val1}\n')
+            add_str(result, key, val1, ' - ')
+            add_str(result, key, val2, ' + ')
     result.append('}')
     result = ''.join(result)
     return result.lower()
+
+
+def add_str(sourse, key, value, sumbol):
+    return sourse.append(f'{sumbol}{key}: {value}\n')
