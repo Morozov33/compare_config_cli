90ebf3cceb81f46855c2fc4a0bd04f0f46a28cab
Dmitrii Morozov <33morozov@gmail.com>
2022-06-07 10:35:12 +0300

    Refactoring gendeff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,29 +3,22 @@ import json
 
 def generate_diff(file_1, file_2):
     result = ['{\n']
-    result_value = ''
     in_file1 = json.load(open(file_1))
     in_file2 = json.load(open(file_2))
     keys = sorted(list(set(in_file1.keys()).union(set(in_file2.keys()))))
     for key in keys:
         val1 = in_file1.get(key)
         val2 = in_file2.get(key)
-        is_val1 = False if val1 is None else True
-        is_val2 = False if val2 is None else True
-        if val1 == val2 and is_val1 and is_val2:
-            result_value = f'   {key}: {val1}\n'
-            result.append(result_value)
-        elif val1 != val2 and is_val1 and is_val2:
-            result_value = f' - {key}: {val1}\n'
-            result.append(result_value)
-            result_value = f' + {key}: {val2}\n'
-            result.append(result_value)
-        elif is_val2 and not is_val1:
-            result_value = f' + {key}: {val2}\n'
-            result.append(result_value)
-        elif not is_val2 and is_val1:
-            result_value = f' - {key}: {val1}\n'
-            result.append(result_value)
+        if val1 is not None and val2 is not None:
+            if val1 == val2:
+                result.append(f'   {key}: {val1}\n')
+            else:
+                result.append(f' - {key}: {val1}\n')
+                result.append(f' + {key}: {val2}\n')
+        elif val1 is None and val2 is not None:
+            result.append(f' + {key}: {val2}\n')
+        else:
+            result.append(f' - {key}: {val1}\n')
     result.append('}')
     result = ''.join(result)
     return result.lower()
